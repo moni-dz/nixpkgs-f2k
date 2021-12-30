087b555407f62b160eb44f233e042e0585a0ef55
fortuneteller2k <lythe1107@gmail.com>
2021-12-30 23:59:32 +0800

    change build plan for iosevka-ft-qp

--- pkgs/iosevka-ft/build-plan-qp.nix
+++ pkgs/iosevka-ft/build-plan-qp.nix
@@ -2,50 +2,46 @@
   [buildPlans.iosevka-ft-qp]
   family = "Iosevka FT QP"
   spacing = "quasi-proportional"
-  serifs = "sans"
+  serifs = "slab"
   no-cv-ss = false
   export-glyph-names = true
 
     [buildPlans.iosevka-ft-qp.variants]
-    inherits = "ss14"
+    inherits = "ss03"
 
       [buildPlans.iosevka-ft-qp.variants.design]
-      capital-j = "serifless"
-      capital-k = "curly-serifless"
-      capital-r = "curly"
-      g = "single-storey-serifless"
-      k = "curly-serifless"
-      q = "earless-corner"
-      y = "cursive"
-      lower-alpha = "crossing"
-      lower-delta = "flat-top"
-      lower-lambda = "straight-turn"
-      cyrl-capital-ka = "curly-serifless"
-      cyrl-ka = "curly-serifless"
+      g = "single-storey-serifed"
+      t = "flat-hook"
+      y = "cursive-motion-serifed"
+      lower-lambda = "curly-turn"
       cyrl-capital-u = "cursive"
-      three = "two-arcs"
+      zero = "dotted"
+      one = "base-long-top-serif"
+      two = "curly-neck"
+      three = "flat-top"
       four = "closed"
-      six = "closed-contour"
-      seven = "straight-serifless"
-      eight = "crossing-asymmetric"
-      nine = "closed-contour"
+      five = "oblique-upper-left-bar"
+      seven = "curly-serifed"
       underscore = "above-baseline"
-      caret = "high"
-      paren = "normal"
+      paragraph-sign = "low"
       brace = "curly-flat-boundary"
-      at = "fourfold"
+      ampersand = "upper-open"
+      percent = "rings-continuous-slash"
+      lig-ltgteq = "slanted"
       ascii-single-quote = "raised-comma"
       ascii-grave = "raised-turn-comma"
-      question = "smooth"
+      question = "corner"
+      punctuation-dot = "square"
 
       [buildPlans.iosevka-ft-qp.variants.italic]
-      a = "double-storey-serifless"
-      f = "serifless"
+      f = "diagonal-tailed"
+      g = "single-storey-serifless"
+      x = "curly-serifed"
 
       [buildPlans.iosevka-ft-qp.variants.oblique]
-      f = "serifless"
-      cyrl-ef = "cursive"
+      f = "diagonal-tailed"
+      g = "single-storey-serifed"
 
-  [buildPlans.iosevka-ft-qp.ligations]
-  inherits = "dlig"
+    [buildPlans.iosevka-ft-qp.ligations]
+    inherits = "dlig"
 ''
