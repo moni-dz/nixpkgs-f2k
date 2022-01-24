551164aab110349445716338cabd717aa49541dd
fortuneteller2k <lythe1107@gmail.com>
2022-01-24 20:45:03 +0800

    update cargoDeps hash

--- flake.nix
+++ flake.nix
@@ -109,7 +109,7 @@
 
             cargoDeps = old.cargoDeps.overrideAttrs (_: {
               inherit src;
-              outputHash = "sha256-tY5sle1YUlUidJcq7RgTzkPsGLnWyG/3rtPqy2GklkY=";
+              outputHash = "sha256-nbzGpxGvwFD7zLP2DApAihkCrgeIBIXOEnO8+bYb7M8=";
             });
           }));
 
