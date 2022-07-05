46ab3d518061c5b0147c6a717b5ebb8d83e89cc1
fortuneteller2k <lythe1107@gmail.com>
2022-07-05 13:23:12 +0000

    i should have really looked at stdenvAdapters more

--- flake.nix
+++ flake.nix
@@ -202,20 +202,27 @@
 
                 (lib.optimizeStdenv "armv9-a" clangStdenv).mkDerivation { ... }
               */
-              optimizeStdenv = march: prev.stdenvAdapters.addAttrsToDerivation {
-                NIX_CFLAGS_COMPILE = prev.lib.concatStringsSep " " (commonFlags ++ [ "-march=${march}" ]);
-              };
+              optimizeStdenv = march: prev.stdenvAdapters.withCFlags (commonFlags ++ "-march=${march}");
+
+              /*
+                Example:
+
+                { lib, stdenv, ... }:
+
+                (lib.optimizeStdenvWithNative stdenv).mkDerivation { ... }
+              */
+              optimizeStdenvWithNative = stdenv: prev.stdenvAdapters.impureUseNativeOptimizations (prev.stdenvAdapters.withCFlags commonFlags stdenv);
             in
             {
-              lib = prev.lib.extend (_: _: { inherit optimizeStdenv; }); 
+              lib = prev.lib.extend (_: _: { inherit optimizeStdenv optimizeStdenvWithNative; });
               optimizedV4Stdenv = final.lib.optimizeStdenv "x86-64-v4" prev.stdenv;
               optimizedV3Stdenv = final.lib.optimizeStdenv "x86-64-v3" prev.stdenv;
               optimizedV2Stdenv = final.lib.optimizeStdenv "x86-64-v2" prev.stdenv;
-              optimizedNativeStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" final.lib.optimizeStdenv "native" prev.stdenv;
+              optimizedNativeStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" optimizeStdenvWithNative prev.stdenv;
               optimizedV4ClangStdenv = final.optimizeStdenv "x86-64-v4" prev.llvmPackages_14.stdenv;
               optimizedV3ClangStdenv = final.optimizeStdenv "x86-64-v3" prev.llvmPackages_14.stdenv;
               optimizedV2ClangStdenv = final.optimizeStdenv "x86-64-v2" prev.llvmPackages_14.stdenv;
-              optimizedNativeClangStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" final.lib.optimizeStdenv "native" prev.llvmPackages_14.stdenv;
+              optimizedNativeClangStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" optimizeStdenvWithNative prev.llvmPackages_14.stdenv;
             };
 
           default = final: prev: with self.overlays;
