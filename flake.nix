9bf3afec0bdf541122a18ecf7f7075d9628a6ad1
fortuneteller2k <lythe1107@gmail.com>
2023-06-18 00:33:44 +0800

    refactor awesome

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     nixpkgs-fmt.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = args@{ self, emacs, nixpkgs, crane, nixpkgs-fmt, ... }:
+  outputs = { self, emacs, nixpkgs, crane, nixpkgs-fmt, ... }:
     let
       linux = [ "x86_64-linux" ];
       darwin = [ "aarch64-darwin" "x86_64-darwin" ];
@@ -220,69 +220,49 @@
             stevenblack-blocklist-git = (getPackage "stevenblack-blocklist" prev).src;
           };
 
-          window-managers = _: prev: {
-            awesome-git = (prev.awesome.overrideAttrs (_:
-              let
-                package = getPackage "awesome" prev;
-                extraGIPackages = with prev; [ networkmanager upower playerctl ];
-              in
-              {
-                inherit (package) src version;
-                patches = [ ];
+          window-managers = final: prev:
+            let
+              mkAwesome = name: pkgs:
+                let
+                  package = getPackage name pkgs;
+                  extraGIPackages = with pkgs; [ networkmanager upower playerctl ];
+                in
+                (pkgs.awesome.override { gtk3Support = true; }).overrideAttrs (_: {
+                  inherit (package) src version;
 
-                postPatch = ''
-                  patchShebangs tests/examples/_postprocess.lua
-                  patchShebangs tests/examples/_postprocess_cleanup.lua
-                '';
+                  patches = [ ];
 
-                GI_TYPELIB_PATH =
-                  let
-                    mkTypeLibPath = pkg: "${pkg}/lib/girepository-1.0";
-                    extraGITypeLibPaths = prev.lib.forEach extraGIPackages mkTypeLibPath;
-                  in
-                  prev.lib.concatStringsSep ":" (extraGITypeLibPaths ++ [ (mkTypeLibPath prev.pango.out) ]);
-              })).override {
-              gtk3Support = true;
+                  postPatch = ''
+                    patchShebangs tests/examples/_postprocess.lua
+                    patchShebangs tests/examples/_postprocess_cleanup.lua
+                  '';
+
+                  GI_TYPELIB_PATH =
+                    let
+                      mkTypeLibPath = pkg: "${pkg}/lib/girepository-1.0";
+                      extraGITypeLibPaths = prev.lib.forEach extraGIPackages mkTypeLibPath;
+                    in
+                    prev.lib.concatStringsSep ":" (extraGITypeLibPaths ++ [ (mkTypeLibPath prev.pango.out) ]);
+                });
+            in
+            {
+              awesome-git = mkAwesome "awesome" prev;
+              awesome-composite-git = mkAwesome "awesome-composite" prev;
+
+              awesome-luajit-git = final.awesome-git.override {
+                lua = prev.luajit;
+                gtk3Support = true;
+              };
+
+              # Yes, it's a *compositor* because of how Wayland works, I can't be bothered.
+              river-git =
+                let
+                  package = getPackage "river" prev;
+                in
+                prev.river.overrideAttrs (_: {
+                  inherit (package) src version;
+                });
             };
-
-            awesome-composite-git = (prev.awesome.overrideAttrs (_:
-              let
-                package = getPackage "awesome-composite" prev;
-                extraGIPackages = with prev; [ networkmanager upower playerctl ];
-              in
-              {
-                inherit (package) src version;
-                patches = [ ];
-
-                postPatch = ''
-                  patchShebangs tests/examples/_postprocess.lua
-                  patchShebangs tests/examples/_postprocess_cleanup.lua
-                '';
-
-                GI_TYPELIB_PATH =
-                  let
-                    mkTypeLibPath = pkg: "${pkg}/lib/girepository-1.0";
-                    extraGITypeLibPaths = prev.lib.forEach extraGIPackages mkTypeLibPath;
-                  in
-                  prev.lib.concatStringsSep ":" (extraGITypeLibPaths ++ [ (mkTypeLibPath prev.pango.out) ]);
-              })).override {
-              gtk3Support = true;
-            };
-
-            awesome-luajit-git = prev.awesome-git.override {
-              lua = prev.luajit;
-              gtk3Support = true;
-            };
-
-            # Yes, it's a *compositor* because of how Wayland works, I can't be bothered.
-            river-git =
-              let
-                package = getPackage "river" prev;
-              in
-              prev.river.overrideAttrs (_: {
-                inherit (package) src version;
-              });
-          };
 
           stdenvs = final: prev:
             let
