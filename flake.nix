ddb320c7a06c3a8fb8b05c0fd6d2348876b74bee
fortuneteller2k <lythe1107@gmail.com>
2023-06-18 15:52:04 +0800

    testing

--- flake.nix
+++ flake.nix
@@ -218,6 +218,10 @@
               prev.callPackage ./pkgs/mpv-discord/script.nix { inherit (package) src version; };
 
             stevenblack-blocklist-git = (getPackage "stevenblack-blocklist" prev).src;
+
+            prowovider = prev.darwin.apple_sdk_11_0.callPackage ./pkgs/prowovider {
+              inherit (prev.darwin.apple_sdk_11_0.frameworks) SystemConfiguration;
+            };
           };
 
           window-managers = final: prev:
@@ -321,6 +325,7 @@
           darwin = final: prev: with self.overlays;
             (fonts final prev)
             // (editors final prev)
+            // (misc final prev)
             // (terminal-emulators final prev)
             // (stdenvs final prev);
 
---
+++ pkgs/prowovider/default.nix
@@ -0,0 +1,34 @@
+{ lib, stdenv, rustPlatform, fetchFromGitLab, SystemConfiguration }:
+
+rustPlatform.buildRustPackage {
+  pname = "prowovider";
+  version = "6a5b1b69b5cd37e831feab3b56928afbfaa433de";
+
+  src = fetchFromGitLab {
+    owner = "elise";
+    repo = "PrOWOvider";
+    rev = "6a5b1b69b5cd37e831feab3b56928afbfaa433de";
+    hash = "sha256-deZ9pPd1Uot1E1TD0rnENXQSfL0v4w+u+8PxKB/WxR0=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+
+    outputHashes = {
+      "jsonwebkey-0.3.5" = "sha256-KqFnXyhuHZLLx6gExbP2O3RjaoYIOYsCb5QKodv8eIA=";
+      "openid-provider-0.3.0" = "sha256-QWrQESKB+6CJoiu+ddYQTtCbi11c/7UBeG8SCT3sS9Q=";
+    };
+  };
+
+  nativeBuildInputs = [];
+  buildInputs = lib.optionals stdenv.isDarwin [ SystemConfiguration ];
+
+  # needs an actual setup
+  doCheck = false;
+
+  meta = with lib; {
+    description = "";
+    homepage = "";
+    license = licenses.asl20;
+  };
+}
