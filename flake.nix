c5e3818ac49bc63e6491072b70428ac9c8582c43
fortuneteller2k <lythe1107@gmail.com>
2023-07-03 23:59:16 +0800

    ensure using the pkgs with custom config set

--- flake.nix
+++ flake.nix
@@ -13,38 +13,37 @@
     nixpkgs-fmt.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, parts, nixpkgs, emacs, nixpkgs-fmt, ... }:
-    parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ./overlays ];
-      systems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
+  outputs = inputs@{ self, parts, ... }: parts.lib.mkFlake { inherit inputs; } {
+    imports = [ ./overlays ];
+    systems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
 
-      _module.args.getPackage = pname: pkgs: (pkgs.callPackage ./_sources/generated.nix { }).${pname};
+    _module.args.getPackage = pname: pkgs: (pkgs.callPackage ./_sources/generated.nix { }).${pname};
 
-      perSystem = { lib, system, pkgs, ... }: {
-        _module.args.pkgs = import nixpkgs {
+    perSystem = { lib, system, ... }:
+      let
+        pkgs = import inputs.nixpkgs {
           inherit system;
           allowUnfree = true;
           allowUnsupportedSystem = true;
-
-          overlays = [
-            self.overlays.default
-            emacs.overlay
-          ];
+          overlays = [ self.overlays.default inputs.emacs.overlay ];
         };
+      in
+      {
+        _module.args.pkgs = pkgs;
 
         packages =
           if lib.hasSuffix "darwin" system
           then builtins.removeAttrs (self.overlays.darwin pkgs pkgs) [ "lib" ]
           else builtins.removeAttrs (self.overlays.linux pkgs pkgs) [ "lib" ];
 
-        formatter = nixpkgs-fmt.defaultPackage.${system};
+        formatter = inputs.nixpkgs-fmt.defaultPackage.${system};
       };
 
-      flake = {
-        nixosModules = {
-          nvidia-exec = import ./modules/nvidia-exec.nix;
-          stevenblack = import ./modules/stevenblack.nix;
-        };
+    flake = {
+      nixosModules = {
+        nvidia-exec = import ./modules/nvidia-exec.nix;
+        stevenblack = import ./modules/stevenblack.nix;
       };
     };
+  };
 }
