216fe5bbad77c0f497265d85694eb5b99750e374
fortuneteller2k <lythe1107@gmail.com>
2023-07-21 02:28:22 +0800

    apple silicon stdenvs

--- overlays/stdenvs.nix
+++ overlays/stdenvs.nix
@@ -4,7 +4,7 @@
   flake.overlays.stdenvs = final: prev:
     let
       commonFlags = [
-        "-O3"
+        "-Ofast"
         "-pipe"
         "-ffloat-store"
         "-fexcess-precision=fast"
@@ -26,32 +26,36 @@
       ];
 
       /*
-                Example:
+        Example:
 
-                { lib, clangStdenv, ... }:
+        { lib, clangStdenv, ... }:
 
-                (lib.optimizeStdenv "armv9-a" clangStdenv).mkDerivation { ... }
-              */
+        (lib.optimizeStdenv "armv9-a" clangStdenv).mkDerivation { ... }
+        (lib.optimizeStdenv' "cortex-a78" clangStdenv).mkDerivation { ... }
+      */
       optimizeStdenv = march: prev.stdenvAdapters.withCFlags (commonFlags ++ [ "-march=${march}" ]);
+      optimizeStdenv' = mcpu: prev.stdenvAdapters.withCFlags (commonFlags ++ [ "-mcpu=${mcpu}" ]);
 
       /*
-                Example:
+        Example:
 
-                { lib, stdenv, ... }:
+        { lib, stdenv, ... }:
 
-                (lib.optimizeStdenvWithNative stdenv).mkDerivation { ... }
-              */
+        (lib.optimizeStdenvWithNative stdenv).mkDerivation { ... }
+      */
       optimizeStdenvWithNative = stdenv: prev.stdenvAdapters.impureUseNativeOptimizations (prev.stdenvAdapters.withCFlags commonFlags stdenv);
     in
     {
-      lib = prev.lib.extend (_: _: { inherit optimizeStdenv optimizeStdenvWithNative; });
+      lib = prev.lib.extend (_: _: { inherit optimizeStdenv optimizeStdenv' optimizeStdenvWithNative; });
       optimizedV4Stdenv = final.lib.optimizeStdenv "x86-64-v4" prev.stdenv;
       optimizedV3Stdenv = final.lib.optimizeStdenv "x86-64-v3" prev.stdenv;
       optimizedV2Stdenv = final.lib.optimizeStdenv "x86-64-v2" prev.stdenv;
-      optimizedNativeStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" optimizeStdenvWithNative prev.stdenv;
-      optimizedV4ClangStdenv = final.lib.optimizeStdenv "x86-64-v4" prev.llvmPackages_14.stdenv;
-      optimizedV3ClangStdenv = final.lib.optimizeStdenv "x86-64-v3" prev.llvmPackages_14.stdenv;
-      optimizedV2ClangStdenv = final.lib.optimizeStdenv "x86-64-v2" prev.llvmPackages_14.stdenv;
-      optimizedNativeClangStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" optimizeStdenvWithNative prev.llvmPackages_14.stdenv;
+      optimizedNativeStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" final.lib.optimizeStdenvWithNative prev.stdenv;
+      optimizedV4ClangStdenv = final.lib.optimizeStdenv "x86-64-v4" prev.llvmPackages_latest.stdenv;
+      optimizedV3ClangStdenv = final.lib.optimizeStdenv "x86-64-v3" prev.llvmPackages_latest.stdenv;
+      optimizedV2ClangStdenv = final.lib.optimizeStdenv "x86-64-v2" prev.llvmPackages_latest.stdenv;
+      optimizedNativeClangStdenv = prev.lib.warn "using native optimizations, forfeiting reproducibility" final.lib.optimizeStdenvWithNative prev.llvmPackages_latest.stdenv;
+      appleM1Stdenv = final.lib.optimizeStdenv' "apple-m1" prev.llvmPackages_latest.stdenv;
+      appleM2Stdenv = final.lib.optimizeStdenv' "apple-m2" prev.llvmPackages_latest.stdenv;
     };
 }
